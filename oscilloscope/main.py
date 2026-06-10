2abe2ffbbb888b2c5edd0f58c0638b8c1c5599ec
Chris Doble <chris@chrisdoble.com>
2026-06-10 13:13:22 +1000

    Update the Python script to read the average voltage of channel 1 on the scope

--- oscilloscope/main.py
+++ oscilloscope/main.py
@@ -1,4 +1,5 @@
 import pyvisa
+import time
 import warnings
 
 resource_manager = pyvisa.ResourceManager('@py')
@@ -11,4 +12,33 @@ with warnings.catch_warnings():
 
 assert len(resources) == 1, "expected exactly 1 matching resource"
 scope = resource_manager.open_resource(resources[0])
-print(scope.query("*IDN?"))
+
+# Stop the scope
+scope.write(':STOP')
+
+# Clear the screen
+scope.write(':CLEar')
+
+# Show channel 1
+scope.write(':CHANnel1:DISPLAY ON')
+
+# Set the offset of channel 1 to 0 V
+scope.write(':CHANnel1:OFFSet 0')
+
+# Set the vertical scale of channel 1 to 1 V/div
+scope.write(':CHANnel1:SCALe 1')
+
+# Clear all measurement items
+scope.write(':MEASure:CLEar')
+
+# Run the scope
+scope.write(':RUN')
+
+# Start calculating the average voltage of channel 1
+scope.write(':MEASure:ITEM? VAVG,CHANnel1')
+
+# Give the scope some time to sample
+time.sleep(0.5)
+
+# Query the average voltage of channel 1
+print(scope.query(':MEASure:ITEM? VAVG,CHANnel1'))
